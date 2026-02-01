c24d93ebf33cdecea70c57d64c72bcf89111e911
pgundlupetvenkatesh <pratikgv1@gmail.com>
2026-02-01 14:46:25 -0500

    update Config class

--- config/config.py
+++ config/config.py
@@ -1,15 +1,18 @@
 import os
 from typing import List
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    BASE_URL: str = os.getenv('TMDB_BASE_URL', "https://api.themoviedb.org/3")
-    API_KEY: str = os.getenv('TMDB_API_KEY', "")
+    BASE_URL: str = os.getenv("TMDB_BASE_URL")
+    API_KEY: str = os.getenv("TMDB_API_KEY")
     TIMEOUT: int = int(os.getenv("TMDB_TIMEOUT", "30"))  # in seconds
-    ACCOUNT_ID: str = os.getenv("TMDB_ACCOUNT_ID", "")
-    MOVIE_ID: str = os.getenv("TMDB_MOVIE_ID", "")
-    SESSION_ID: str = os.getenv("TMDB_SESSION_ID", "]")
-    AUTH_TOKEN: str = os.getenv("TMDB_AUTH_TOKEN", "")
-    REQUEST_TOKEN: str = os.getenv("TMDB_REQ_TOKEN", "")
+    ACCOUNT_ID: str = os.getenv("TMDB_ACCOUNT_ID", "12016691")
+    MOVIE_ID: str = os.getenv("TMDB_MOVIE_ID", "346698")
+    SESSION_ID: str = os.getenv("TMDB_SESSION_ID")
+    AUTH_TOKEN: str = os.getenv("TMDB_AUTH_TOKEN")
+    REQUEST_TOKEN: str = os.getenv("TMDB_REQ_TOKEN")
 
     @classmethod
     def validate(cls, required: List[str] = None) -> None:
