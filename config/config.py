3db98d8528ae4647f4d0b56fb15899a981e90aef
pgundlupetvenkatesh <pratikgv1@gmail.com>
2026-02-01 15:17:27 -0500

    update github sctions yml file and create .env file and save secrets

--- config/config.py
+++ config/config.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    BASE_URL: str = os.getenv("TMDB_BASE_URL")
-    API_KEY: str = os.getenv("TMDB_API_KEY")
+    BASE_URL: str = os.getenv("TMDB_BASE_URL", "https://api.themoviedb.org/3")
     TIMEOUT: int = int(os.getenv("TMDB_TIMEOUT", "30"))  # in seconds
     ACCOUNT_ID: str = os.getenv("TMDB_ACCOUNT_ID", "12016691")
     MOVIE_ID: str = os.getenv("TMDB_MOVIE_ID", "346698")
+
+    API_KEY: str = os.getenv("TMDB_API_KEY")
     SESSION_ID: str = os.getenv("TMDB_SESSION_ID")
     AUTH_TOKEN: str = os.getenv("TMDB_AUTH_TOKEN")
     REQUEST_TOKEN: str = os.getenv("TMDB_REQ_TOKEN")
