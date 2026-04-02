be6cffebe8ef4fd342453697fc3de591e0105b89
pgundlupetvenkatesh <pratikgv1@gmail.com>
2026-04-02 16:53:04 -0400

    update poeple tests

--- tests/people/test_details.py
+++ tests/people/test_details.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from loguru import logger
 
@@ -7,7 +8,11 @@ from tests.helpers import *
 
 TEST_DATA = load_test_data("test_data.yaml")
 
+@allure.epic("TMDB API")
+@allure.feature("People")
 class TestDetails(FieldAssertions):
+    @allure.story("Get Person Details")
+    @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.parametrize('person_details', TEST_DATA['get_person_details']['valid'])
     def test_get_person_details(self, get_api_instance, load_schema, person_details):
         """
@@ -19,39 +24,47 @@ class TestDetails(FieldAssertions):
         :param person_details: Dictionary containing test parameters for a valid case.
         """
         logger.info(f"Random person ID picked: {person_details['person_id']}")
+        allure.dynamic.title(f"Get details for person ID: {person_details['person_id']}")
         people_api = get_api_instance('people_api')
-        response = people_api.get_person_details(person_details['person_id'])
-        res_body = response.data
 
-        assert_http_response(response, {
-            'exp_status_code': person_details['status_code'],
-            'exp_max_elp_seconds': person_details['exp_max_elp_secs'],
-            'exp_req_method': person_details['exp_get_req_method'],
-            'exp_content_type': person_details['exp_content_type'],
-            'exp_url_contains': 'person',
-            'exp_req_reason': person_details['reason']
-        })
+        with allure.step(f"Send GET request for person ID {person_details['person_id']}"):
+            response = people_api.get_person_details(person_details['person_id'])
+            res_body = response.data
+
+        with allure.step("Validate HTTP response metadata"):
+            assert_http_response(response, {
+                'exp_status_code': person_details['status_code'],
+                'exp_max_elp_seconds': person_details['exp_max_elp_secs'],
+                'exp_req_method': person_details['exp_get_req_method'],
+                'exp_content_type': person_details['exp_content_type'],
+                'exp_url_contains': 'person',
+                'exp_req_reason': person_details['reason']
+            })
 
         # response structure validation
-        self.assert_list_field(res_body, 'also_known_as')
+        with allure.step("Validate response structure"):
+            self.assert_list_field(res_body, 'also_known_as')
 
-        self.assert_str_field(res_body, 'biography')
-        self.assert_str_field(res_body, 'birthday')
-        self.assert_str_field(res_body, 'imdb_id')
-        self.assert_str_field(res_body, 'known_for_department')
-        self.assert_str_field(res_body, 'name')
-        self.assert_str_field(res_body, 'place_of_birth')
-        self.assert_path_field(res_body, 'profile_path')
+            self.assert_str_field(res_body, 'biography')
+            self.assert_str_field(res_body, 'birthday')
+            self.assert_str_field(res_body, 'imdb_id')
+            self.assert_str_field(res_body, 'known_for_department')
+            self.assert_str_field(res_body, 'name')
+            self.assert_str_field(res_body, 'place_of_birth')
+            self.assert_path_field(res_body, 'profile_path')
 
-        self.assert_bool_field(res_body, 'adult')
+            self.assert_bool_field(res_body, 'adult')
 
-        self.assert_int_field(res_body, 'gender')
-        self.assert_int_field(res_body, 'id')
-        self.assert_float_field(res_body, 'popularity')
+            self.assert_int_field(res_body, 'gender')
+            self.assert_int_field(res_body, 'id')
+            self.assert_float_field(res_body, 'popularity')
 
         # Validate against JSON schema
-        load_schema('person_details_schema').model_validate(res_body)
+        with allure.step("Validate against Pydantic schema"):
+            load_schema('person_details_schema').model_validate(res_body)
 
+    @allure.story("Get Invalid Person Details")
+    @allure.severity(allure.severity_level.NORMAL)
     @pytest.mark.parametrize('invalid_person_details', TEST_DATA['get_person_details']['invalid'])
     def test_get_person_details_invalid(self, get_api_instance, load_schema, invalid_person_details):
         """
@@ -63,23 +76,30 @@ class TestDetails(FieldAssertions):
         :param invalid_person_details: Dictionary containing test parameters for invalid case.
         """
         people_api = get_api_instance('people_api')
-        response = people_api.get_person_details(invalid_person_details['person_id'])
-        res_body = response.data
+        allure.dynamic.title(f"Get details for invalid person ID: {invalid_person_details['person_id']}")
 
-        assert_http_response(response, {
-            'exp_status_code': invalid_person_details['status_code'],
-            'exp_max_elp_seconds': invalid_person_details['exp_max_elp_secs'],
-            'exp_req_method': invalid_person_details['exp_get_req_method'],
-            'exp_content_type': invalid_person_details['exp_content_type'],
-            'exp_url_contains': 'person',
-            'exp_req_reason': invalid_person_details['reason']
-        })
+        with allure.step(f"Send GET request for person ID {invalid_person_details['person_id']}"):
+            response = people_api.get_person_details(invalid_person_details['person_id'])
+            res_body = response.data
+
+        with allure.step("Validate HTTP response metadata"):
+            assert_http_response(response, {
+                'exp_status_code': invalid_person_details['status_code'],
+                'exp_max_elp_seconds': invalid_person_details['exp_max_elp_secs'],
+                'exp_req_method': invalid_person_details['exp_get_req_method'],
+                'exp_content_type': invalid_person_details['exp_content_type'],
+                'exp_url_contains': 'person',
+                'exp_req_reason': invalid_person_details['reason']
+            })
 
         # response structure validation
-        self.assert_str_field(res_body, 'status_message')
-        self.assert_bool_field(res_body, 'success')
-        self.assert_int_field(res_body, 'status_code')
+        with allure.step("Validate response structure"):
+            self.assert_str_field(res_body, 'status_message')
+            self.assert_bool_field(res_body, 'success')
+            self.assert_int_field(res_body, 'status_code')
 
         if 'success' in res_body:
             assert res_body['success'] is False, f"{self._test_name}: Expected 'success' to be False for invalid person ID"
-            load_schema('generic_schema').model_validate(res_body)
+
+            with allure.step("Validate against Pydantic schema"):
+                load_schema('generic_schema').model_validate(res_body)
